036ddfd5b8b2f3d40bb64bbd8cbe9ded47318139
phinx110 <popp16771@gmail.com>
2017-11-14 16:33:51 +0100

    implimented edge_match and slice_image

--- src/lib/puzzleSolver.py
+++ src/lib/puzzleSolver.py
@@ -8,17 +8,21 @@ class puzzleSolver:
         self.__orb = cv2.ORB_create()
         self.__bf= cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    def handle_pieces(self):
-        pieces = self.puzzle.pieces
-        for i in pieces:
-            kp1, des1 = self.__orb.detectAndCompute(i,None)
-            for j in pieces:
-                kp2, des2 = self.__orb.detectAndCompute(j,None)
-                matches = self.__bf.match(des1, des2)
-                matches = sorted(matches, key=lambda x:x.distance)
-                output = cv2.drawMatches(i, kp1, j, kp2, matches[:5], None, flags=2)
-                cv2.imshow('matched', output)
-                cv2.waitKey(1000)
-                cv2.destroyWindow('matched')
-
+    def edge_match(self, edge1, edge2):
+        weight = 0
+        max_it = np.min(len(edge1), len(edge1))
+        for i in range(max_it):
+            weight += abs(edge1[i]- edge2[i])
+        return weight
+    
+    def slice_image(self, dim):
+        size_x = int(len(self.puzzle.puzzle[0])/dim)
+        size_y = int(len(self.puzzle.puzzle)/dim)
+        slices = []        
+        for i in range(dim):
+            for j in range(dim):
+                slices.append(self.puzzle.puzzle[j*size_y:(j+1)*size_y, i*size_x:(i+1)*size_x])                
+        self.puzzle.pieces = slices
+            
+        
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
 import lib.puzzle as puzzle
 import lib.puzzleSolver as puzzleSolver
 
-puzz1 = puzzle.Puzzle('puzzles/Tiles/tiles_scrambled/tiles_scrambled_5x5_08.png')
-puzz1.show()
 
-puzz1.contours()
-puzz1.draw_contours()
-puzz1.calc_pieces(margin=8, draw=True)
-puzz1.show_pieces()
+def show(img, time=0):
+        cv2.imshow('Puzzle',img)
+        cv2.waitKey(time)
+        cv2.destroyWindow('Puzzle')
+
+
+puzz1 = puzzle.Puzzle('../puzzles/Tiles/tiles_rotated/tiles_rotated_5x5_02.png')
+#puzz1.show()
+
+#puzz1.contours()
+#puzz1.draw_contours()
+#puzz1.calc_pieces(margin=8, draw=True)
+#puzz1.show_pieces()
+
 
 solver = puzzleSolver.puzzleSolver(puzz1)
+solver.slice_image(3)
+#p = solver.puzzle.pieces
+
+#show(p[0])
+
+
+solver.puzzle.show_pieces()
