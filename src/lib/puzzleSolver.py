3f6743d2eaa47ba0799017de94ccca8469737e38
phinx110 <33456216+phinx110@users.noreply.github.com>
2017-11-16 17:25:53 +0100

    Tiles matching upgrade
    *optimised slice_image
    *slice_image now needs vertical and horizontal dimentions
    *compare_rgb_pixels added
    *compare_rgb_slices that uses compare_rgb_pixels added
    *get_edges_nesw that gives border slisec of noth east south west borders

--- src/lib/puzzleSolver.py
+++ src/lib/puzzleSolver.py
@@ -8,21 +8,45 @@ class puzzleSolver:
         self.__orb = cv2.ORB_create()
         self.__bf= cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    def edge_match(self, edge1, edge2):
+    def edge_match_grey(self, edge1, edge2):
         weight = 0
         max_it = np.min(len(edge1), len(edge1))
         for i in range(max_it):
             weight += abs(edge1[i]- edge2[i])
         return weight
     
-    def slice_image(self, dim):
-        size_x = int(len(self.puzzle.puzzle[0])/dim)
-        size_y = int(len(self.puzzle.puzzle)/dim)
-        slices = []        
-        for i in range(dim):
-            for j in range(dim):
-                slices.append(self.puzzle.puzzle[j*size_y:(j+1)*size_y, i*size_x:(i+1)*size_x])                
-        self.puzzle.pieces = slices
-            
+    def slice_image(self, dimv, dimh):
+        size_x = int(len(self.puzzle.puzzle)/dimv)
+        size_y = int(len(self.puzzle.puzzle[0])/dimh)
         
-
+        slices = [np.empty([size_x, size_y,3]) for _ in range(dimv*dimh)]
+        for i in range(dimh):
+            for j in range(dimv):
+                slices[i*dimv+j] = self.puzzle.puzzle[j*size_x:(j+1)*size_x, i*size_y:(i+1)*size_y]
+        self.puzzle.pieces = slices
+    
+    def compare_rgb_pixels(pixel1, pixel2):
+        b = abs(int(pixel1[0])-int(pixel2[0]))
+        r = abs(int(pixel1[1])-int(pixel2[1]))
+        g = abs(int(pixel1[2])-int(pixel2[2]))
+        return b+r+g
+        
+    def compare_rgb_slices(slice1, slice2):
+        weight = 0
+        if(len(slice1)==len(slice2)):
+            for i in range(len(slice1)):
+                weight = puzzleSolver.compare_rgb_pixels(slice1[i], slice2[i])
+        else:
+            return 256*3*max(len(slices1), len(slices2))
+        return weight
+        
+    def get_edges_nesw(self, piece_number):
+        north = self.puzzle.pieces[piece_number][0,0:]
+        east = self.puzzle.pieces[piece_number][0:,-1]
+        south = self.puzzle.pieces[piece_number][-1,0:]
+        west = self.puzzle.pieces[piece_number][0:,0]
+        return north, east, south, west
+        
+        
+        
+        
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import lib.puzzle as puzzle
 import lib.puzzleSolver as puzzleSolver
-
+import numpy as np
 
 def show(img, time=0):
         cv2.imshow('Puzzle',img)
@@ -8,7 +8,7 @@ def show(img, time=0):
         cv2.destroyWindow('Puzzle')
 
 
-puzz1 = puzzle.Puzzle('../puzzles/Tiles/tiles_rotated/tiles_rotated_5x5_02.png')
+puzz1 = puzzle.Puzzle('../puzzles/Tiles/tiles_shuffled/tiles_shuffled_2x3_02.png')
 #puzz1.show()
 
 #puzz1.contours()
@@ -18,10 +18,23 @@ puzz1 = puzzle.Puzzle('../puzzles/Tiles/tiles_rotated/tiles_rotated_5x5_02.png')
 
 
 solver = puzzleSolver.puzzleSolver(puzz1)
-solver.slice_image(3)
-#p = solver.puzzle.pieces
-
-#show(p[0])
+solver.slice_image(2,3)
 
 
-solver.puzzle.show_pieces()
+im1 = 3
+im2 = 0
+
+n1, e1, s1, w1 = solver.give_edges_NESW(im1)
+n2, e2, s2, w2 = solver.give_edges_NESW(im2)
+img = np.hstack([solver.puzzle.pieces[im1], solver.puzzle.pieces[im2]])
+
+
+
+weight = puzzleSolver.puzzleSolver.compare_rgb_slices(e1,w2)
+print(weight)
+
+show(img)
+
+
+
+#solver.puzzle.show_pieces()
